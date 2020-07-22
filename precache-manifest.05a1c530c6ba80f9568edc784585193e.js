b8c4fc6968883f847eeec5dd857e75ec95c0ec21
Dekki Akki <akhil4dp@gmail.com>
2020-07-22 14:19:27 +0530

    Updates

---
+++ precache-manifest.05a1c530c6ba80f9568edc784585193e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9951391479957c88f5e5a0630957f631",
+    "url": "/Face-Recognition-website/index.html"
+  },
+  {
+    "revision": "69b8e771d58ceadfc471",
+    "url": "/Face-Recognition-website/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0f7aa5ecab5c72be62f3",
+    "url": "/Face-Recognition-website/static/css/main.c0b08c81.chunk.css"
+  },
+  {
+    "revision": "69b8e771d58ceadfc471",
+    "url": "/Face-Recognition-website/static/js/2.1c851c35.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/Face-Recognition-website/static/js/2.1c851c35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f7aa5ecab5c72be62f3",
+    "url": "/Face-Recognition-website/static/js/main.da707a2f.chunk.js"
+  },
+  {
+    "revision": "5e765602b092e08fa468",
+    "url": "/Face-Recognition-website/static/js/runtime-main.c74bb378.js"
+  },
+  {
+    "revision": "277776624fc9fc86f01f29c4250bf0d1",
+    "url": "/Face-Recognition-website/static/media/logo.27777662.png"
+  }
+]);
